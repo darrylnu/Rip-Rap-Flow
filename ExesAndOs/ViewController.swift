dec9d06f2cc5198564e991fa2adfea187e61de91
Darryl Nunn <darrylnu@gmail.com>
2016-02-05 16:02:31 -0500

    Game Board created, Pieces are placed on board. will not overwrite. when piece is placed, gameState is updated

--- ExesAndOs/ViewController.swift
+++ ExesAndOs/ViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentPlayer = 1
+    
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    
+    
+    @IBOutlet var gamePiece: UIButton!
+    
+    
+    @IBAction func placePiece(sender: AnyObject) {
+        
+        if currentPlayer == 1 && gameState[sender.tag] == 0 {
+        
+            sender.setImage(UIImage(named:"cross.png"), forState: .Normal)
+            gameState[sender.tag] = currentPlayer
+            currentPlayer = 2
+            
+        } else if currentPlayer == 2 && gameState[sender.tag] == 0 {
+            
+            sender.setImage(UIImage(named:"nought.png"), forState: .Normal)
+            gameState[sender.tag] = currentPlayer
+            currentPlayer = 1
+
+        }
+        print(gameState)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
